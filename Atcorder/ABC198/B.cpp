c2d513a88aec5c63c78cb810efacaf0940f18042
daiki <daiki.20000727@gmail.com>
2021-05-01 11:25:49 +0900

    add code

--- Atcorder/ABC198/B.cpp
+++ Atcorder/ABC198/B.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < n; i++)
 #define repr(i, n) for(int i = n; i >= 0; i--)
 #define rep(i, m, n) for(int i = m; i < n; i++)
 #define inf 2e9
@@ -8,15 +9,11 @@ template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } retu
 using namespace std;
 typedef long long ll;
 int main(){
-    int N, A[100],B[100];
+    string N;
     cin >> N;
-    rep(i,0,N) cin >> A[i];
-    rep(i,0,N) cin >> B[i];
-    int left = -inf, right=inf;
-    rep(i,0,N){
-        max(left,A[i]);
-        min(right,B[i]);
-    }
-    int ans=max(0,right-left+1);
-    cout << ans << endl;
+    while(*N.rbegin()=='0') N=N.substr(0,N.length()-1);
+    string NN = N;
+    reverse(ALL(NN));
+    if(N == NN) cout << "YES" << endl;
+    else cout << "NO" << endl;
 }
--- Atcorder/ABC198/C.cpp
+++ Atcorder/ABC198/C.cpp
@@ -1,22 +1,19 @@
 #include <bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < n; i++)
 #define repr(i, n) for(int i = n; i >= 0; i--)
 #define rep(i, m, n) for(int i = m; i < n; i++)
 #define inf 2e9
-#define ALL(v) v.begin(), v.end()
+#define all(v) v.begin(), v.end()
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 using namespace std;
 typedef long long ll;
 int main(){
-    int N, A[100],B[100];
-    cin >> N;
-    rep(i,0,N) cin >> A[i];
-    rep(i,0,N) cin >> B[i];
-    int left = -inf, right=inf;
-    rep(i,0,N){
-        max(left,A[i]);
-        min(right,B[i]);
-    }
-    int ans=max(0,right-left+1);
+    double R, X, Y;
+    cin >> R >> X >> Y;
+    double distance = sqrt(X*X+Y*Y);
+    int ans = ceil(distance / R);
+    if(ans == 1 && distance != R) ans++;
+
     cout << ans << endl;
 }
