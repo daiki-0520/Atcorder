a138457b69148cc715a46102d6dbc513e27f5d31
daiki <daiki.20000727@gmail.com>
2021-06-18 16:50:17 +0900

    add code

--- Atcorder/ABC182/B.py
+++ Atcorder/ABC182/B.py
@@ -1,1 +1,13 @@
-n = int
+N = int(input())
+a = list(map(int, input().split()))
+
+result = 0
+for i in range(2 , 1000):
+    t = 0
+    for x in a:
+        if x % i == 0:
+            t += 1
+    result = max(result, t)
+
+print(result)
+
